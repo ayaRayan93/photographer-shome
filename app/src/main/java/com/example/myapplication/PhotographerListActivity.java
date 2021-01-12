98ebf4fc3fe1596f50b7915dba1cd819054cbd1e
aya <aya.rayan1011@gmail.com>
2021-01-12 13:28:44 -0800

    model and parser

--- app/src/main/java/com/example/myapplication/PhotographerListActivity.java
+++ app/src/main/java/com/example/myapplication/PhotographerListActivity.java
@@ -17,7 +17,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.example.myapplication.dummy.DummyContent;
 
 import java.util.List;
 
@@ -41,71 +40,6 @@ public class PhotographerListActivity extends AppCompatActivity {
 
         View recyclerView = findViewById(R.id.photographer_list);
         assert recyclerView != null;
-        setupRecyclerView((RecyclerView) recyclerView);
     }
 
-    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
-        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(this, DummyContent.ITEMS, mTwoPane));
-    }
-
-    public static class SimpleItemRecyclerViewAdapter
-            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
-
-        private final PhotographerListActivity mParentActivity;
-        private final List<DummyContent.DummyItem> mValues;
-        private final boolean mTwoPane;
-        private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                DummyContent.DummyItem item = (DummyContent.DummyItem) view.getTag();
-
-                    Context context = view.getContext();
-                    Intent intent = new Intent(context, MainActivity.class);
-                   // intent.putExtra(PhotographerDetailFragment.ARG_ITEM_ID, item.id);
-
-                    context.startActivity(intent);
-
-            }
-        };
-
-        SimpleItemRecyclerViewAdapter(PhotographerListActivity parent,
-                                      List<DummyContent.DummyItem> items,
-                                      boolean twoPane) {
-            mValues = items;
-            mParentActivity = parent;
-            mTwoPane = twoPane;
-        }
-
-        @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.photographer_list_content, parent, false);
-            return new ViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(final ViewHolder holder, int position) {
-            holder.mIdView.setText(mValues.get(position).id);
-            holder.mContentView.setText(mValues.get(position).content);
-
-            holder.itemView.setTag(mValues.get(position));
-            holder.itemView.setOnClickListener(mOnClickListener);
-        }
-
-        @Override
-        public int getItemCount() {
-            return mValues.size();
-        }
-
-        class ViewHolder extends RecyclerView.ViewHolder {
-            final TextView mIdView;
-            final TextView mContentView;
-
-            ViewHolder(View view) {
-                super(view);
-                mIdView = (TextView) view.findViewById(R.id.id_text);
-                mContentView = (TextView) view.findViewById(R.id.content);
-            }
-        }
-    }
 }
---
+++ app/src/main/java/com/example/myapplication/model/Photographer.java
@@ -0,0 +1,70 @@
+package com.example.myapplication.model;
+
+public class Photographer {
+
+    String Photographer_ID,PhotographerName,Location,Facebook,Behance,WhatsApp,Instagram,ProfileImage;
+
+    public String getPhotographer_ID() {
+        return Photographer_ID;
+    }
+
+    public void setPhotographer_ID(String photographer_ID) {
+        Photographer_ID = photographer_ID;
+    }
+
+    public String getPhotographerName() {
+        return PhotographerName;
+    }
+
+    public void setPhotographerName(String photographerName) {
+        PhotographerName = photographerName;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String location) {
+        Location = location;
+    }
+
+    public String getFacebook() {
+        return Facebook;
+    }
+
+    public void setFacebook(String facebook) {
+        Facebook = facebook;
+    }
+
+    public String getBehance() {
+        return Behance;
+    }
+
+    public void setBehance(String behance) {
+        Behance = behance;
+    }
+
+    public String getWhatsApp() {
+        return WhatsApp;
+    }
+
+    public void setWhatsApp(String whatsApp) {
+        WhatsApp = whatsApp;
+    }
+
+    public String getInstagram() {
+        return Instagram;
+    }
+
+    public void setInstagram(String instagram) {
+        Instagram = instagram;
+    }
+
+    public String getProfileImage() {
+        return ProfileImage;
+    }
+
+    public void setProfileImage(String profileImage) {
+        ProfileImage = profileImage;
+    }
+}
